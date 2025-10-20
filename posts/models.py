c19b0850427c2fff25db0e2fbd50de42d4fc2169
Alexey Karatchentsev <karatchentsev@yandex.ru>
2025-10-20 19:46:32 +0300

    testing X200

--- posts/models.py
+++ posts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Post(models.Model):
-    # some changes for commit - testing
+    # some changes for commit - from X200
     text = models.TextField()
 
     def __str__(self):
