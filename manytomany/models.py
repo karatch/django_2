ec56ef9eb68f934f532672719733cf52f1afafb7
Alexey Karachentsev <alexey.karachentsev@vniira.spb.su>
2025-10-22 09:35:03 +0300

    added new models

---
+++ manytomany/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Course(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=30)
+    courses = models.ManyToManyField(Course)
+
+    def __str__(self):
+        return self.name
