a0466c45a3be218c83a0a145c60fcab8c57a751d
Alexey Karatchentsev <karatchentsev@yandex.ru>
2025-10-22 20:51:39 +0300

    added further models 4

--- posts/models.py
+++ posts/models.py
@@ -6,3 +6,25 @@ class Post(models.Model):
 
     def __str__(self):
         return self.text[:50]
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200, help_text='Введите жанр книги')
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(blank=True, null=True)
+    date_of_death = models.DateField(blank=True, null=True)
+
+    class Meta:
+        ordering = ['last_name',]
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    summary = models.CharField(max_length=255, help_text='Введите описание книги')
+    isbn = models.CharField(max_length=13, help_text='Введите код ISBN')
+    genre = models.ManyToManyField(Genre, help_text='Введите жанр книги')
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
