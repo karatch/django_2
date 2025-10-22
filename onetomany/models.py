d7a458e84b63aa3acffd362bb7016d738ce28dbe
Alexey Karachentsev <alexey.karachentsev@vniira.spb.su>
2025-10-22 09:57:18 +0300

    added new models 2

--- onetomany/models.py
+++ onetomany/models.py
@@ -8,3 +8,21 @@ class Product(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
     price = models.IntegerField()
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField()
+
+
+class Topic(models.Model):
+    title = models.CharField(max_length=255)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    created = models.DateTimeField()
+    likes = models.ManyToManyField(User)
