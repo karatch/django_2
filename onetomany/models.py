56b6b6da082d550319011f97a2c6a5ec2dcd1cbd
Alexey Karachentsev <alexey.karachentsev@vniira.spb.su>
2025-10-23 07:46:39 +0300

    some changes

--- onetomany/models.py
+++ onetomany/models.py
@@ -10,23 +10,19 @@ class Product(models.Model):
     price = models.IntegerField()
 
 
-class Publisher(models.Model):
-    name = models.CharField(max_length=30)
-    address = models.CharField(max_length=50)
-    city = models.CharField(max_length=60)
-    post_index = models.CharField(max_length=10)
-    country = models.CharField(max_length=50)
-    website = models.URLField()
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
 
 
-class Author(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=40)
-    email = models.EmailField()
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField()
 
 
-class Book(models.Model):
-    title = models.CharField(max_length=100)
-    authors = models.ManyToManyField(Author)
-    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
-    publication_date = models.DateField()
+class Topic(models.Model):
+    title = models.CharField(max_length=255)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    created = models.DateTimeField()
+    likes = models.ManyToManyField(User)
