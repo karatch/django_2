e8b3efa0a4980fc5ddd3926125d2ba368f09487c
Alexey Karachentsev <alexey.karachentsev@vniira.spb.su>
2025-10-27 08:37:44 +0300

    played around

--- orm_model/views.py
+++ orm_model/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from orm_model.models import Worker
 
 def index(request):
-    people = Worker.objects.all().order_by('-id')
+    # people = Worker.objects.all()
+    people = Worker.objects.filter(id=25)
     return render(request, 'index.html', {'people': people})
